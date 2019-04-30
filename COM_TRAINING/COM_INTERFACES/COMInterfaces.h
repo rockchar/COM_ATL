9208c80b33ba7e12f0adf712d4aa9be21773a7cf
rohit <rohitsinha050481@gmail.com>
2019-04-30 13:48:04 +0530

    Create COMInterfaces.h

---
+++ COM_TRAINING/COM_INTERFACES/COMInterfaces.h
@@ -0,0 +1,13 @@
+#pragma once4
+/*
+COM,OLE and ACTIVEX Relationship:
+COM is the backbone of all the OLE technologies.OLE 1.0 was a 16-Bit technology that appeared around 1991.
+It enabled software applications to share information through linking and embedding. Certain applications 
+may export visual objects to other applications. A host application may embed this object inside itself 
+(Which produced a copy of the original data) or reference through a link(which is the connection to thr 
+original data source)
+
+A classic example of this technology is an MS-Word document containing an embedded MS-Excel spreadsheet.
+
+Active X is the current blanket name for any COM-based technology
+*/

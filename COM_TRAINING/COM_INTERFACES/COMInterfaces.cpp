b9b5feb962651cec976e7ed22d8f3988a77ec1e2
rohit <rohitsinha050481@gmail.com>
2019-04-30 14:35:41 +0530

    Added Interfaces

---
+++ COM_TRAINING/COM_INTERFACES/COMInterfaces.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "COMInterfaces.h"
+
+HRESULT CoHexagon::QueryInterface(REFIID riid, void ** ppv)
+{
+	return E_NOTIMPL;
+}
--- COM_TRAINING/COM_INTERFACES/COMInterfaces.h
+++ COM_TRAINING/COM_INTERFACES/COMInterfaces.h
@@ -1,5 +1,6 @@
-#pragma once4
-/*
+#pragma once
+#include <Windows.h>
+/*******************************************************************************************************
 COM,OLE and ACTIVEX Relationship:
 COM is the backbone of all the OLE technologies.OLE 1.0 was a 16-Bit technology that appeared around 1991.
 It enabled software applications to share information through linking and embedding. Certain applications 
@@ -10,4 +11,27 @@ original data source)
 A classic example of this technology is an MS-Word document containing an embedded MS-Excel spreadsheet.
 
 Active X is the current blanket name for any COM-based technology
-*/
+*********************************************************************************************************/
+/*------------------------------------------------------------------------------------------------------*/
+/*************************************** INTERFACES *****************************************************
+                                
+COM is all about interfaces - the only way in which clients and servers can communicate is through the 
+COM interfaces.
+An interface is a collection of semantically related functions, which describe a single and unique 
+behavior that may be supported by a class.
+Interfaces have - no state,no Implementation
+Every COM interface (standard or custom) must ultimately derive from the core COM interface - IUnknown
+*********************************************************************************************************/
+//Could also derive from another IUnknown derived interface as well 
+
+interface IDraw : public IUnknown
+{
+	// this also brings in virtual definitions of QueryInterface()
+	// addref() and release()
+	virtual void Draw() = 0;
+};
+
+class CoHexagon : public IDraw {
+public:
+	HRESULT _stdcall QueryInterface(REFIID riid, void ** ppv);
+};
